995713448aa4bff8b2f7e820dde9a622df2f93a4
akt-mac <akitamac+develop@gmail.com>
2020-01-17 17:35:20 +0900

    login function completed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_14_054659) do
+ActiveRecord::Schema.define(version: 2020_01_17_080838) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_01_14_054659) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
