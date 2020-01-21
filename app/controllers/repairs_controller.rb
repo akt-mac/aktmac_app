79a655d7dfb423ea5ac2ce5d0791b93df2528d1a
akt-mac <akitamac+develop@gmail.com>
2020-01-21 12:05:03 +0900

    add repair new

--- app/controllers/repairs_controller.rb
+++ app/controllers/repairs_controller.rb
@@ -1,6 +1,30 @@
 class RepairsController < ApplicationController
-  before_action :logged_in_user, only: %i(index)
+  before_action :logged_in_user, only: %i(index new create show edit update destroy)
 
   def index
   end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+    def repair_params
+      params.require(:repair).permit()
+    end
 end
