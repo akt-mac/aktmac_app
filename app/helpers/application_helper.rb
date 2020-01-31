1b7bb6b7a1f25cabdbb4d49fcd07db6bea614af8
akt-mac <akitamac+develop@gmail.com>
2020-01-31 11:53:17 +0900

    add order of index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,12 +10,12 @@ module ApplicationHelper
   end
 
   # 登録から2日以内はnewマークを表示
-  def new_arrivals(created_at)
-    text = "new"
-    if created_at.since(1.day) > Time.now
-      text
-    end
-  end
+  # def new_arrivals(created_at)
+  #   text = "new"
+  #   if created_at.since(1.day) > Time.now
+  #     text
+  #   end
+  # end
 
   # 値が無い場合は未登録表示
   def blank_text(text)
