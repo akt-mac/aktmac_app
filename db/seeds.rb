cc8b032b88c76d7ec1963c6a0770e3934d7dfa18
akt-mac <akitamac+develop@gmail.com>
2020-03-23 17:05:13 +0900

    update gemfile for Heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,17 +7,17 @@ User.create!(name: "管理者",
              admin: true,
              editor: true)
 
-15.times do |n|
-  name  = Faker::Name.name
-  email = "sample-#{n+1}@email.com"
-  password = "password"
-  User.create!(name: name,
-               email: email,
-               password: password,
-               password_confirmation: password)
-end
-
-puts "Sample user creation completed!"
+# 15.times do |n|
+#   name  = Faker::Name.name
+#   email = "sample-#{n+1}@email.com"
+#   password = "password"
+#   User.create!(name: name,
+#                email: email,
+#                password: password,
+#                password_confirmation: password)
+# end
+#
+# puts "Sample user creation completed!"
 
 MachineCategory.create!(
   [
@@ -32,33 +32,33 @@ MachineCategory.create!(
 
 puts "Sample machine_category creation completed!"
 
-50.times do |n|
-  reception_day = Faker::Date.between(from: 4.month.ago, to: 1.day.from_now)
-  r_name = [Faker::Name.name, Faker::Company.name]
-  customer_name = r_name.sample
-  r_add = [Faker::Address.city, Faker::Address.street_address]
-  address = "秋田市" + r_add[0] + r_add[1]
-  phone_number = "018" + rand(800..899).to_s + rand(1000..9999).to_s
-  mobile_phone_number = ["070", "080", "090"].sample + rand(1000..9999).to_s + rand(1000..9999).to_s
-  machine_model = Faker::Types.character.upcase + Faker::Types.character.upcase + Faker::Number.between(from: 330, to: 580).to_s
-  condition = "症状#{n+1}。テスト、" + Faker::Lorem.sentence
-  category = ["チェンソー", "刈払機", "モア", "薪割機", "除雪機", "その他"].sample
-  progress = 1
-  repair_staff = Faker::Name.name
-  note = "備考#{n+1}。テスト、" + Faker::Lorem.sentence
-  reception_number = n + 100
-  Repair.create!(reception_day: reception_day,
-                 customer_name: customer_name,
-                 address: address,
-                 phone_number: phone_number,
-                 mobile_phone_number: mobile_phone_number,
-                 machine_model: machine_model,
-                 condition: condition,
-                 category: category,
-                 progress: progress,
-                 repair_staff: repair_staff,
-                 note: note,
-                 reception_number: reception_number)
-end
-
-puts "Sample repair creation completed!"
+# 50.times do |n|
+#   reception_day = Faker::Date.between(from: 4.month.ago, to: 1.day.from_now)
+#   r_name = [Faker::Name.name, Faker::Company.name]
+#   customer_name = r_name.sample
+#   r_add = [Faker::Address.city, Faker::Address.street_address]
+#   address = "秋田市" + r_add[0] + r_add[1]
+#   phone_number = "018" + rand(800..899).to_s + rand(1000..9999).to_s
+#   mobile_phone_number = ["070", "080", "090"].sample + rand(1000..9999).to_s + rand(1000..9999).to_s
+#   machine_model = Faker::Types.character.upcase + Faker::Types.character.upcase + Faker::Number.between(from: 330, to: 580).to_s
+#   condition = "症状#{n+1}。テスト、" + Faker::Lorem.sentence
+#   category = ["チェンソー", "刈払機", "モア", "薪割機", "除雪機", "その他"].sample
+#   progress = 1
+#   repair_staff = Faker::Name.name
+#   note = "備考#{n+1}。テスト、" + Faker::Lorem.sentence
+#   reception_number = n + 100
+#   Repair.create!(reception_day: reception_day,
+#                  customer_name: customer_name,
+#                  address: address,
+#                  phone_number: phone_number,
+#                  mobile_phone_number: mobile_phone_number,
+#                  machine_model: machine_model,
+#                  condition: condition,
+#                  category: category,
+#                  progress: progress,
+#                  repair_staff: repair_staff,
+#                  note: note,
+#                  reception_number: reception_number)
+# end
+#
+# puts "Sample repair creation completed!"
