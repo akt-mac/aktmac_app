5bad72e8c29531a135db7b2ab0bff249c962588c
akt-mac <akitamac+develop@gmail.com>
2020-03-24 18:15:01 +0900

    progress

--- db/migrate/20200324085831_add_progress_to_repairs.rb
+++ db/migrate/20200324085831_add_progress_to_repairs.rb
@@ -1,5 +1,18 @@
 class AddProgressToRepairs < ActiveRecord::Migration[5.2]
-  def change
-    add_column :repairs, :progress, :integer, default: 1
+  # def change
+  #   add_column :repairs, :progress, :integer, default: 1
+  # end
+  def up
+    # 環境ごとにマイグレーションを分ける
+    if Rails.env.development? || Rails.env.test?
+      change_column :repairs_url, :progress, :integer
+    else Rails.env.production?
+      # 本番環境はusingオプションを追加
+      change_column :repairs, :progress, 'progress USING CAST(integer AS progress)'
+    end
+  end
+
+  def down
+    change_column :repairs, :progress, :integer
   end
 end
