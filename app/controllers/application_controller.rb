1cd7b44c226a42c766e0682393be8333936992b6
akt-mac <akitamac+develop@gmail.com>
2020-03-26 14:42:46 +0900

    fixed edit access error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -50,6 +50,8 @@ class ApplicationController < ActionController::Base
   end
 
   def correct_or_admin_user
+    set_user
+    @user = User.find(params[:user_id]) if @user.blank?
     unless current_user?(@user) || current_user.admin?
       redirect_to root_url
       flash[:danger] = "アクセス権限がありません。"
